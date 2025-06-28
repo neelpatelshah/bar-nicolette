f747b319e3120d530e396afbfea1f99b733ea4c2
Neel Patel-Shah <neelpatelshah@gmail.com>
2025-06-28 19:22:00 -0400

    fuck all yall

--- app/cleanout/page.tsx
+++ app/cleanout/page.tsx
@@ -11,21 +11,21 @@ export default function Storytime() {
       <div className="w-full flex flex-col justify-center items-center gap-4">
         <p className="text-zinc-300 font-philosopher text-sm px-4">
           Pick a vibe or two, a couple qualities, and tell me how strong you
-          want it to be and I will try to make you something fun.
+          want it to be and I will try to make you something fun
         </p>
-        <h2 className="text-xl font-semibold font-macondo">Vibe</h2>
-        <div className="text-zinc-400 mb-3 font-arvo text-sm">
+        <h2 className="text-xl font-semibold font-macondo">Flavors</h2>
+        <div className="text-zinc-400 mb-3 font-arvo text-sm text-center">
           Floral, plums, mace, jasmine tea, nutty, fruity, bitter, herbal,
-          tropical.
+          tropical
         </div>
-        <h2 className="text-xl font-semibold font-macondo">Qualities</h2>
+        <h2 className="text-xl font-semibold font-macondo">Vibe</h2>
         <div className="text-zinc-400 mb-3 font-arvo text-sm">
           Sweet, dry, juicy, refreshing
         </div>
         <h2 className="text-xl font-semibold font-macondo">Strength</h2>
         <div className="text-zinc-400 mb-3 font-arvo text-sm text-center">
           Basically do you want a glass of booze, something normal, or something
-          light.
+          light
         </div>
       </div>
     </main>
